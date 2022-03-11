cc9a3008287be3962b1a367dc8fc0246002ace0d
krylex-365 <63896058+krylex-365@users.noreply.github.com>
2022-03-11 18:43:50 +0700

    project styling component

---
+++ 01-starting-setup/src/components/Class.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Class extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is class component</h2>
+            </div>
+        );
+    }
+}
+
+export default Class;
---
+++ 01-starting-setup/src/components/Functional.js
@@ -0,0 +1,9 @@
+const Functional = () => {
+    return (
+        <div>
+            <h2>This is functional component</h2>
+        </div>
+    );
+}
+
+export default Functional;
